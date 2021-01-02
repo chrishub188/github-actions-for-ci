c15a7a6f5059b76150be445821f7372ae0546cde
1522784 <florian.kutz@t-online.de>
2021-01-02 12:03:17 +0100

    feat: fix html and add dockerfile

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/public/index.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.css'));
+});
+
+app.get('/public/main.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/main.js'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
